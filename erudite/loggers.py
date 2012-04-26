dd868c20090025b09de6ebb7e7e70595e59f6da3
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-04-26 15:37:06 -0500

    add stathat presence only logging

--- erudite/loggers.py
+++ erudite/loggers.py
@@ -36,6 +36,10 @@ class StatHatLogger(NoOpLogger):
             ['--stathat-prefix'],
             dict(type=str, help="Prefix for stathat stats"),
         ),
+        (
+            ['--stathat-presence-only'],
+            dict(action="store_true", help="Only store presence of a line, not it's content (requires --stathat-prefix)")
+        ),
     ]
 
     def __init__(self, options):
@@ -51,15 +55,28 @@ class StatHatLogger(NoOpLogger):
             print 'stathat-key is a required argument when using the stathat logger'
             sys.exit(2)
 
+        # initialize a stathat instance
         self.stathat = StatHat(options.stathat_key)
         print 'Initialized stathat for %s' % options.stathat_key
 
-        if options.stathat_prefix is not None:
-            self.prefix = '%s.' % options.stathat_prefix
-            print '\t... with prefix %s' % options.stathat_prefix
+        # deal with what kind of data to send
+        self.presence_only = options.stathat_presence_only
+        if self.presence_only:
+            self.prefix = options.stathat_prefix
+            if self.prefix is None:
+                print 'stathat-prefix is a required argument when using presence only'
+                sys.exit(2)
+
+            print '\t... logging presence only, with key %s' % self.prefix
+
         else:
-            self.prefix = ''
-            print '\t... with no prefix'
+            if options.stathat_prefix is not None:
+                self.prefix = '%s.' % options.stathat_prefix
+                print '\t... with prefix %s' % options.stathat_prefix
+            else:
+                self.prefix = ''
+                print '\t... with no prefix'
+
 
 
     def log(self, values):
@@ -68,6 +85,6 @@ class StatHatLogger(NoOpLogger):
             print 'stathat logger can only use parsers with single arguments'
             sys.exit(2)
 
-        key = self.prefix + values[0]
+        key = self.prefix + values[0] if not self.presence_only else self.prefix
         result = self.stathat.count(key, 1)
-        print 'Logging "%s". Result: %s' % (key, result)
+        print 'Logging "%s". Successful: %s' % (key, result)
