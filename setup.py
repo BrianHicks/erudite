7e25ee000439be86e0932fa4c570816a1da29236
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-04-26 14:29:25 -0500

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'Erudite',
-    version = '0.0.1',
+    version = '1.0.0',
     packages = find_packages(),
 
     install_requires = [
